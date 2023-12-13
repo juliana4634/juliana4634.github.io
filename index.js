e8f3a3329f3c58e1813ea458cb2b2143d89df356
julianamedalla <142897449+juliana4634@users.noreply.github.com>
2023-12-13 10:47:04 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function openModal() {
+    document.getElementById('overlay').style.display = 'block';
+    document.getElementById('modal').style.display = 'block';
+}
+function closeModal() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('modal').style.display = 'none';
+}
+
+
+
+
+
+
